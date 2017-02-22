92f974cf55435dbc0e97feb75a46e87dc159f40e
Tanner Welsh <tannerwelsh@gmail.com>
2017-02-22 07:37:20 -0500

    Refactor goal model into own lib file

---
+++ lib/goal.rb
@@ -0,0 +1,31 @@
+require 'front_matter_parser'
+
+class Goal
+  attr_reader :metadata, :content
+
+  def initialize(opts)
+    @metadata = opts[:metadata]
+    @content = opts[:content]
+  end
+
+  def to_s
+    metadata['title']
+  end
+
+  def to_json
+    metadata.merge({ content: content }).to_json
+  end
+
+  def [](key)
+    metadata[key]
+  end
+
+  def self.load_all(files)
+    files
+      .reject { |f| f =~ /template/i }
+      .map do |file|
+        parsed = FrontMatterParser.parse_file(file)
+        Goal.new(metadata: parsed.front_matter, content: parsed.content)
+      end
+  end
+end
