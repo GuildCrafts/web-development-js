9d599de0b8c19dc789a1786dcfe3f8eb8cbc9b67
Tanner Welsh <tannerwelsh@gmail.com>
2017-04-19 13:41:10 -0400

    Change XP Value -> Base XP

--- lib/goal.rb
+++ lib/goal.rb
@@ -15,7 +15,7 @@ class Goal
     @source_file = opts[:source_file]
 
     @metadata['url'] = url if @source_file
-    @metadata['xp_value'] = xp_value
+    @metadata['base_xp'] = base_xp
     @metadata['dynamic'] ||= false # default to false
   end
 
@@ -35,8 +35,8 @@ class Goal
     self.class.base_url + filename + '.html'
   end
 
-  def xp_value
-    # If you change this, make sure to also change the computed value in _layouts/goal.html
+  def base_xp
+    # If you change this, make sure to also change the computed value in _includes/goal_badges.html
     self['team_size'].to_i * self['level'].to_i * 50
   end
 
