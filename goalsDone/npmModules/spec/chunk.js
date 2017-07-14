a66d5da7f21f591d7626b883af0973bcfb7911e8
Tanner Welsh <tannerwelsh@gmail.com>
2017-07-14 09:20:07 -0400

    Revert "work in a couple lodash projects"
    This reverts commit 8244bec44044e43e2380eafe70765d455a935955.

--- goalsDone/npmModules/spec/chunk.js
+++
@@ -1,3 +0,0 @@
-module.exports = function chunk(arr, num) {
-  new array =
-}
--- goalsDone/npmModules/test/add.test.js
+++
@@ -1,9 +0,0 @@
-const {expect} = require("chai")
-const add = require('../spec/add')
-
-
-describe("add", function() {
-  it('should add two numbers', function(){
-    expect(add(1, 2)).to.eql(3)
-  })
-})
